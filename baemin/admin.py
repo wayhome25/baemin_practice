1c315239a82e58f33664e375c4ee829a99b2f028
wayhome25 <siwabada@gmail.com>
2017-06-16 22:19:04 +0900

    baemin.models.Order 모델 생성 및 admin 등록

--- baemin/admin.py
+++ baemin/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Item, Shop
+from .models import Item, Order, Shop
 
 @admin.register(Shop)
 class ShopAdmin(admin.ModelAdmin):
@@ -11,3 +11,8 @@ class ShopAdmin(admin.ModelAdmin):
 class ItemAdmin(admin.ModelAdmin):
     list_display = ['id', 'shop', 'name', 'price']
     list_display_links = ['shop', 'name']
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'shop', 'user']
+    list_display_links = ['shop']
--- baemin/models.py
+++ baemin/models.py
@@ -1,13 +1,24 @@
+from django.conf import settings
 from django.db import models
-
 
 class Shop(models.Model):
     name = models.CharField(max_length=20)
     tel = models.CharField(max_length=20)
     addr = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Item(models.Model):
     shop = models.ForeignKey(Shop)
     name = models.CharField(max_length=50)
     price = models.IntegerField()
-    
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    shop = models.ForeignKey(Shop)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    item_set = models.ManyToManyField(Item)
+    created_at = models.DateTimeField(auto_now_add=True)
