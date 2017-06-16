6d7d1b51bbcd18aac5c6490ea77aebac33696579
wayhome25 <siwabada@gmail.com>
2017-06-16 19:51:58 +0900

    baemin.models.Item 모델 생성 및 admin 등록

--- baemin/admin.py
+++ baemin/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import Shop
+from .models import Item, Shop
 
 @admin.register(Shop)
 class ShopAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'tel']
     list_display_links = ['name']
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['id', 'shop', 'name', 'price']
+    list_display_links = ['shop', 'name']
--- baemin/models.py
+++ baemin/models.py
@@ -5,3 +5,9 @@ class Shop(models.Model):
     name = models.CharField(max_length=20)
     tel = models.CharField(max_length=20)
     addr = models.CharField(max_length=100)
+
+class Item(models.Model):
+    shop = models.ForeignKey(Shop)
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    
