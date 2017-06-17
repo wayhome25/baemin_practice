3bf4fa682a4b79b410383c8198865ee6e82bebd3
wayhome25 <siwabada@gmail.com>
2017-06-17 18:50:26 +0900

    가게목록 구현(index view), bootstrap 적용

--- 2nd_practice/baemin/views.py
+++ 2nd_practice/baemin/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-
+from .models import Shop
 
 def index(request):
     return render(request, 'baemin/index.html', {
-        
+        'shop_list': Shop.objects.all(),
     })
