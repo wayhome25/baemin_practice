6a0b5a7869000bcc7ee913941a059a6a7b52fbfd
wayhome25 <siwabada@gmail.com>
2017-06-17 21:35:24 +0900

    모델 기본정렬 변경, naturaltime 필드옵션 nav에 home 메뉴 추가

--- 2nd_practice/baemin/models.py
+++ 2nd_practice/baemin/models.py
@@ -6,6 +6,9 @@ class Shop(models.Model):
     name = models.CharField(max_length=100)
     tel = models.CharField(max_length=20)
     addr = models.CharField(max_length=100)
+    
+    class Meta:
+        ordering = ['-id']
 
     def __str__(self):
         return self.name
@@ -15,6 +18,9 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     price = models.PositiveIntegerField()
 
+    class Meta:
+        ordering = ['-id']
+
     def __str__(self):
         return self.name
 
@@ -23,6 +29,9 @@ class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
     item_set = models.ManyToManyField(Item)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-id']
 
     @property
     def total(self):
