cc9db5481a93bd90d50f7225673f91de8538118c
wayhome25 <siwabada@gmail.com>
2017-06-17 12:31:06 +0900

    layout 구현 및 템플릿 상속 적용

--- baemin/models.py
+++ baemin/models.py
@@ -25,6 +25,9 @@ class Order(models.Model):
     item_set = models.ManyToManyField(Item)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-created_at']
+
     @property
     def total(self):
         return sum(item.price for item in self.item_set.all())
