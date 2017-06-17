f454bcce70a028ab6ad2546ef2ccab88985d9806
wayhome25 <siwabada@gmail.com>
2017-06-17 17:48:56 +0900

    로그아웃 구현

--- 2nd_practice/accounts/urls.py
+++ 2nd_practice/accounts/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     url(r'^login/$', auth_views.login, name='login', kwargs={
         'template_name': 'accounts/login.html'
     }),
+    url(r'^logout/$', auth_views.logout, name='logout', kwargs={
+        'next_page': '/',
+    }),
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^profile/$', views.profile, name='profile'),
 ]
