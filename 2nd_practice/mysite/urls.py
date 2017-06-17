fb6684a290a8aa709f65e69ee066053c00a97227
wayhome25 <siwabada@gmail.com>
2017-06-17 17:33:08 +0900

    baemin 앱 추가

--- 2nd_practice/mysite/urls.py
+++ 2nd_practice/mysite/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('accounts.urls')),
+    url(r'^baemin/', include('baemin.urls', namespace='baemin')),
 ]
