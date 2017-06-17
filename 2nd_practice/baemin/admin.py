832257e1160a3ca56d8553eafba7d8ea7d2a8371
wayhome25 <siwabada@gmail.com>
2017-06-17 18:16:59 +0900

    baemin 앱에 모델 추가 및 admin 등록

--- 2nd_practice/baemin/admin.py
+++ 2nd_practice/baemin/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Shop, Item, Order
 
-# Register your models here.
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ['name', 'tel', 'addr']
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['shop', 'name', 'price']
+    list_filter = ['shop',]
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['shop', 'user', 'created_at']
+    list_filter = ['shop', 'user']
--- 2nd_practice/baemin/models.py
+++ 2nd_practice/baemin/models.py
@@ -1,3 +1,25 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Shop(models.Model):
+    name = models.CharField(max_length=100)
+    tel = models.CharField(max_length=20)
+    addr = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    shop = models.ForeignKey(Shop)
+    name = models.CharField(max_length=100)
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    shop = models.ForeignKey(Shop)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    item_set = models.ManyToManyField(Item)
+    created_at = models.DateTimeField(auto_now_add=True)
