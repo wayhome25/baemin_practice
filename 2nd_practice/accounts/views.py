fb97a6e2a5f1f82f4e4c5b834d6e0a5de8d070a9
wayhome25 <siwabada@gmail.com>
2017-06-17 15:27:47 +0900

    회원가입 구현(Built-in form 활용)

--- 2nd_practice/accounts/views.py
+++ 2nd_practice/accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.contrib.auth.forms import UserCreationForm # NOTE: Built-in forms
+from django.shortcuts import redirect, render
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(settings.LOGIN_URL)
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {
+        'form': form,
+    })
